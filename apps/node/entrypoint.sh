39e06549b0eb5b03ab06de4dfbdf73d9dd07ecd7
Hari Narayanan <smgdark@gmail.com>
2018-10-03 06:53:43 +0530

    total revamp

---
+++ apps/node/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/ash
+set -e
+
+cd /app
+
+if [[ -n "$GIT_URL" ]]; then
+    wget --no-check-certificate -O master.zip $GIT_URL/archive/master.zip && \
+        unzip master.zip && \
+        rm master.zip && \
+        mv $GIT_REPO/* . && \
+        rm -rf $GIT_REPO-master
+fi
+
+npm i
+
+exec "$@"
---
+++ apps/static/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+set -e
+
+cd /app
+
+if [[ -n "$GIT_URL" ]]; then
+    wget --no-check-certificate -O master.zip $GIT_URL/archive/master.zip && \
+        unzip master.zip && \
+        rm master.zip && \
+        mv $GIT_REPO/* . && \
+        rm -rf $GIT_REPO-master
+fi
+
+exec "$@"
