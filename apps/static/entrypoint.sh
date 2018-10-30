1a5c4f491cf7d14a52aac107a4d09ee381e14f21
Hari Narayanan <smgdark@gmail.com>
2018-10-30 10:05:10 +0530

    static logic change

--- apps/static/entrypoint.sh
+++ apps/static/entrypoint.sh
@@ -3,12 +3,4 @@ set -e
 
 cd /app
 
-if [[ -n "$GIT_URL" ]]; then
-    wget --no-check-certificate -O master.zip $GIT_URL/archive/master.zip && \
-        unzip master.zip && \
-        rm master.zip && \
-        mv $GIT_REPO-master/* . && \
-        rm -rf $GIT_REPO-master
-fi
-
 exec "$@"
